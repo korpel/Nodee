7393a2e3af5780a88458f8f0e289d67ca31ff9f3
Korpel <Vozikisant@gmail.com>
2018-12-04 23:47:28 +0200

    playground/json:

--- playground/json.js
+++ playground/json.js
@@ -20,4 +20,12 @@ var originalNote = {
     body : 'Some body'
 };
 
+var originalNoteString = JSON.stringify(originalNote);
+
 fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(note);
