09a7861c05f381867053013cd21a61e5c416e9d9
Korpel <Vozikisant@gmail.com>
2018-12-04 18:57:46 +0200

    note2:

--- note2.js
+++ note2.js
@@ -8,3 +8,13 @@ const file = require('./simpleFile.js');
 
 var command = process.argv[2];
 console.log('command: ', command);
+
+if (command === 'add') {
+    console.log('adding new file');
+} 
+else if (command === 'list') {
+    console.log('list all nodes');
+}
+else {
+    console.log('command not recognized');
+}
