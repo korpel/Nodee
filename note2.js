078b949341784aeceb2f606b938e1ec12a6a7b6d
Korpel <Vozikisant@gmail.com>
2018-12-04 22:45:52 +0200

    everything

--- note2.js
+++ note2.js
@@ -2,13 +2,16 @@
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const file = require('./simpleFile.js');
 
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('command: ', command);
-console.log(process.argv);
+console.log('process: ',process.argv);
+console.log('argv: ', argv);
 
 if (command === 'add') {
     console.log('adding new file');
