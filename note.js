56bf548245690fbe82a58674d56396e54a05e7e1
Korpel <Vozikisant@gmail.com>
2018-12-04 15:41:21 +0200

    note:

--- note.js
+++ note.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const os = require('os');
 const file = require('./simpleFile.js');
-const _ = require('nodash');
+const _ = require('lodash');
 
 var user = os.userInfo();
 
@@ -11,6 +11,10 @@ var res = file.addNote();
 console.log(res);
 var num = file.add(1,2);
 console.log('result:',num);
+
+var filteredArray = _.uniq(['An', 1, 'An',2,3,1,2]);
+console.log(filteredArray);
+
 console.log(_.isString(true));
 console.log(_.isString('Tony'));
 
