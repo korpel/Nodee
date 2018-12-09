ee848faad31baa8d97a63bbc71db60a9347d9da3
Korpel <Vozikisant@gmail.com>
2018-12-09 22:05:34 +0200

    playground/async-basics:

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -1,7 +1,11 @@
 console.log('starting app');
 
 setTimeout(() => {
-    console.log('inside');
+    console.log('inside the first timeout');
 }, 2000);
+
+setTimeout(()=> {
+    console.log('inside the second timeout');
+}, 0);
 
 console.log('finishing app');
