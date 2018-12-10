c8f4804cffccf8f9edd2dc0f09f756d33ce7f99e
Korpel <Vozikisant@gmail.com>
2018-12-10 08:06:39 +0200

    a

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 
 var request = require('request');
+var yargs = require('yargs');
 
+const argv = yargs
+    .options({
+        a: {
+            demand : true,
+            alias : 'address',
+            describe : 'Address demanded for weather app to work',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+    
+    var encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA2u2IBLLeNrzJjy02M2TLJETcjA_LUCwI&address=1301%20lombard%20street%20philadelpia',
+    url:`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA2u2IBLLeNrzJjy02M2TLJETcjA_LUCwI&address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address} location: ${JSON.stringify(body.results[0].geometry.location)}`);
