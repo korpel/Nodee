1df8b243f020c693255a1f01d6536c2c9380661d
Korpel <Vozikisant@gmail.com>
2018-12-10 08:14:55 +0200

    app:

--- app.js
+++ app.js
@@ -21,5 +21,12 @@ request({
     url:`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA2u2IBLLeNrzJjy02M2TLJETcjA_LUCwI&address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
+    if (error) {
+        console.log('cannot connect to google server');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('unable to find address');
+    }
+    else {
     console.log(`Address: ${body.results[0].formatted_address} location: ${JSON.stringify(body.results[0].geometry.location)}`);
+    }
 });
