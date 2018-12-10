42fc59503756b519a6a361d9bb387d98f9fcf5c6
Korpel <Vozikisant@gmail.com>
2018-12-10 07:22:06 +0200

    playground/app:

--- playground/app.js
+++ playground/app.js
@@ -3,5 +3,8 @@ var request = require('request');
 
 
 request({}, (error, response, body)=>{
-
+    url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA2u2IBLLeNrzJjy02M2TLJETcjA_LUCwI&address=1301%20lombard%20street%20philadelpia',
+    json:true
+}(error,response,body) => {
+    console.log(body);
 });
