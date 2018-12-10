5c5c2c27e232f2635a6634c3f3793fe716e323f8
Korpel <Vozikisant@gmail.com>
2018-12-10 07:40:57 +0200

    app:

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA2u2IBLLeNrzJjy02M2TLJETcjA_LUCwI&address=1301%20lombard%20street%20philadelpia',
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Address: ${body.results[0].formatted_address} location: ${JSON.stringify(body.results[0].geometry.location)}`);
 });
