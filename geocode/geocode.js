9564cf2bc68d63227e3f6b7453ab894dd5e6fb5d
Korpel <Vozikisant@gmail.com>
2018-12-11 08:41:27 +0200

    geocode/geocode:

---
+++ geocode/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const credentialsFile = require('./cred');
+
+
+function geocodeAddress (address){
+request({
+    url:`https://maps.googleapis.com/maps/api/geocode/json?key=${credentials}&address=${encodedAddress}`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('cannot connect to google server');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('unable to find address');
+    }
+    else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address} location: ${JSON.stringify(body.results[0].geometry.location)}`);
+    }
+    else {
+        console.log('wtf');
+    }
+});
+}
+
+module.exports = {
+    address
+};
