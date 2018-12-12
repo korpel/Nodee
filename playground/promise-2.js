beb102d8b281a0f28e1f7cd85582a9313934dd6a
Korpel <Vozikisant@gmail.com>
2018-12-12 12:38:27 +0200

    playground/promise-2:

--- playground/promise-2.js
+++ playground/promise-2.js
@@ -1,36 +1,42 @@
 const request = require('request');
 const credentialsFile = require('../geocode/cred2');
+const credentials = credentialsFile.cred;
+
 
 var geocodeAddress = (address) => {
     
     return new Promise((resolve, reject)=>{
+        var encodedAddress = encodeURIComponent(address);
+request({
+    url:`https://maps.googleapis.com/maps/api/geocode/json?key=${credentials}&address=${encodedAddress}`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        reject('cannot connect to google server');
+        console.log();
+    } else if (body.status === 'ZERO_RESULTS') {
+        reject('unable to find address');
+    }
+    else if (body.status === 'OK') {
+        resolve({
+            address: body.results[0].formatted_address,
+            latitude: body.results[0].geometry.location.lat,
+            longtitude: body.results[0].geometry.location.lng,
+            location: body.results[0].geometry.location
 
-        var getWeather = (lat, lng, callback) => {
-            request({
-                url: `https://api.darksky.net/forecast/${credentialsFile.weatherCred}/${lat},${lng}`,
-                json: true
-            }, (error, response, body) => {
-            
-                if (!error & response.statusCode === 200) {
-                   resolve({
-                      temperature: body.currently.temperature,
-                      apparentTemperature: body.currently.apparentTemperature
-                      
-                    });
-            
-                } else {
-                    reject('Unable to fetch weather');
-                }
-            
-            });
-            };
+        })
+    }
+    else {
+        console.log('wtf');
+    }
+});
     }) ;
     
 };
 
-geocodeAddress('19164').then((location)=>{
+geocodeAddress('19164').then((location) => {
     console.log(JSON.stringify(location,undefined,2));
-}, (errorMessage)=>{
+}, (errorMessage) => {
     console.log(errorMessage);
 });
 
