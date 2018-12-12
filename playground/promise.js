5aec7468de861b4da106e2e2eda3350312a58fcf
Korpel <Vozikisant@gmail.com>
2018-12-12 12:15:14 +0200

    playground/promise:

--- playground/promise.js
+++ playground/promise.js
@@ -11,18 +11,24 @@ var asyncAdd =(a,b) => {
     });
 };
 
-var somePromise = new Promise((resolve, reject)=> {
-    setTimeout(()=>{
-        resolve('Hey. It worked');
-    }, 2500);
-    setTimeout(()=> {
-        reject('Unable to fulfill promise');
-    },2500);
-
+asyncAdd(1,true).then((res)=> {
+    console.log('results:',res);
+}, (errorMessage)=>{
+    console.log('failed:', errorMessage);
 });
 
-somePromise.then((message)=>{
-    console.log('Success:', message);
-}, (errorMessage) => {
-    console.log(errorMessage);
-});
+// var somePromise = new Promise((resolve, reject)=> {
+//     setTimeout(()=>{
+//         resolve('Hey. It worked');
+//     }, 2500);
+//     setTimeout(()=> {
+//         reject('Unable to fulfill promise');
+//     },2500);
+
+// });
+
+// somePromise.then((message)=>{
+//     console.log('Success:', message);
+// }, (errorMessage) => {
+//     console.log(errorMessage);
+// });
