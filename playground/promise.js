8e6dfa853f2feddc44e79b77fc486b8ab2a41343
Korpel <Vozikisant@gmail.com>
2018-12-12 12:03:00 +0200

    playground/promise:

--- playground/promise.js
+++ playground/promise.js
@@ -2,6 +2,11 @@
 
 
 var somePromise = new Promise((resolve, reject)=> {
-    resolve('Hey. It worked');
+    //resolve('Hey. It worked');
 
+
+});
+
+somePromise.then((message)=>{
+    console.log('Success:', message);
 });
