330f4d757e01074f3e6c14b60e1350c928164ebf
Korpel <Vozikisant@gmail.com>
2018-12-12 12:42:04 +0200

    app-promise:

--- app-promise.js
+++ app-promise.js
@@ -1,6 +1,5 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+
 
 
 const argv = yargs
@@ -15,21 +14,3 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-
-    geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-        if (errorMessage) {
-            console.log(errorMessage);
-        } else {
-            console.log(results.address);
-            weather.getWeather(results.latitude, results.longtitude, (errorMessage, weatherResults) => {
-                if(errorMessage) {
-                    console.log(errorMessage);
-                } else {
-                    console.log(`It's ${weatherResults.temperature}, but feels like ${weatherResults.apparentTemperature}.`)
-
-                }
-            });
-         }
-    });
-
-    
