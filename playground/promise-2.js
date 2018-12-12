912d9e91121f78710c272c1f5652aad8fe869719
Korpel <Vozikisant@gmail.com>
2018-12-12 12:33:51 +0200

    playground/promise-2:

--- playground/promise-2.js
+++ playground/promise-2.js
@@ -2,21 +2,30 @@ const request = require('request');
 const credentialsFile = require('../geocode/cred2');
 
 var geocodeAddress = (address) => {
-    return new promise(()=>{
-        request({
-        url: `https://api.darksky.net/forecast/${credentialsFile.weatherCred}/${lat},${lng}`,
-        json: true
+    
+    return new Promise((resolve, reject)=>{
 
-           resolve(undefined, {
-              temperature: body.currently.temperature,
-              apparentTemperature: body.currently.apparentTemperature
-              
+        var getWeather = (lat, lng, callback) => {
+            request({
+                url: `https://api.darksky.net/forecast/${credentialsFile.weatherCred}/${lat},${lng}`,
+                json: true
+            }, (error, response, body) => {
+            
+                if (!error & response.statusCode === 200) {
+                   resolve({
+                      temperature: body.currently.temperature,
+                      apparentTemperature: body.currently.apparentTemperature
+                      
+                    });
+            
+                } else {
+                    reject('Unable to fetch weather');
+                }
+            
             });
+            };
+    }) ;
     
-            re('Unable to fetch weather');
-    
-    });
-});
 };
 
 geocodeAddress('19164').then((location)=>{
@@ -27,8 +36,3 @@ geocodeAddress('19164').then((location)=>{
 
 
 
-var getWeather = (lat, lng, callback) => {
-
-}
-
-module.exports.getWeather = getWeather
