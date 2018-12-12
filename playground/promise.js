6cf3675ad967c6f28e55d7adc12ca069c0c531f1
Korpel <Vozikisant@gmail.com>
2018-12-12 12:06:54 +0200

    playground/promise:

--- playground/promise.js
+++ playground/promise.js
@@ -3,7 +3,7 @@
 
 var somePromise = new Promise((resolve, reject)=> {
     setTimeout(()=>{
-        //resolve('Hey. It worked');
+        resolve('Hey. It worked');
         reject('Unable to fulfill promise');
     }, 2500);
 });
