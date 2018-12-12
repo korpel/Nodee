a0bc6b273048e511e970f6633c23e2ae50b84f3e
Korpel <Vozikisant@gmail.com>
2018-12-12 12:05:27 +0200

    playground/promise:

--- playground/promise.js
+++ playground/promise.js
@@ -3,10 +3,11 @@
 
 var somePromise = new Promise((resolve, reject)=> {
     setTimeout(()=>{
-        resolve('Hey. It worked');
-    },2500);
-
+        //resolve('Hey. It worked');
+        reject('Unable to fullfill');
+    }, 2500);
 });
 
 somePromise.then((message)=>{
     console.log('Success:', message);
+})
