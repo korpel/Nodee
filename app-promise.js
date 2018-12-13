daacb93626669282a3c5a0c6b7f0cba488876c6b
Korpel <Vozikisant@gmail.com>
2018-12-13 08:44:36 +0200

    app-promise:

--- app-promise.js
+++ app-promise.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const credentialsFile = require('./geocode/cred2');
 
 const credentials = credentialsFile.cred;
+const credentialsW = credentialsFile.weatherCred;
 
 
 const argv = yargs
@@ -21,13 +22,22 @@ const argv = yargs
 
 
     var encodedAddress = encodeURIComponent(argv.address);
-    var geocodeUrl = `https://mapsgoogleapis.com/maps/api/geocode/json?key=${credentials}&address=${encodedAddress}`;
+    var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${credentials}&address=${encodedAddress}`;
     
     axios.get(geocodeUrl).then((response)=>{
-        console.log(response.data);
+        if(response.data.status === 'ZERO_RESULTS') {
+            throw new Error('Unable to find the address');
+        }
+        var lat = response.data.results[0].geometry.location.lat;
+        var lng = response.data.results[0].geometry.location.lng;
+        var url = `https://api.darksky.net/forecast/${credentialsW}/${lat},${lng}`;
+
+        console.log(response.data.results[0].formatted_address);
     }).catch((e) => {
         if (e.code ==='ENOTFOUND') {
             console.log('unable to have access to the servers')
+        } else {
+            console.log(e.message);
         }
     });
 
