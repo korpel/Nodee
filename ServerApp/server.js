8bc49a5b7612caca3380ba2f69075060c1e26fd7
Korpel <Vozikisant@gmail.com>
2018-12-13 09:45:26 +0200

    ServerApp/server:

--- ServerApp/server.js
+++ ServerApp/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-
+app.use(express.static(__dirname + '/Public'))
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express<\h1>');
     res.send({
