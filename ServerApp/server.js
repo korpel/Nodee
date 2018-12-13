c0e8fdde779dc4880190135d518fd62e82481fbd
Korpel <Vozikisant@gmail.com>
2018-12-13 09:17:09 +0200

    ServerApp/server:

--- ServerApp/server.js
+++ ServerApp/server.js
@@ -5,8 +5,18 @@ var app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello express');
+    // res.send('<h1>Hello express<\h1>');
+    res.send({
+        name : 'Ant',
+        likes : [
+            'biking', 'running', 'fun'
+        ]
+    });
 })
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
 
 
 app.listen(3000);
