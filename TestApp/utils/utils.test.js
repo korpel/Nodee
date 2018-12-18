9184283ca97d0a380dea10c1800962743368ee75
Korpel <Vozikisant@gmail.com>
2018-12-18 19:34:10 +0200

    TestApp/utils/utils.test:

--- TestApp/utils/utils.test.js
+++ TestApp/utils/utils.test.js
@@ -26,4 +26,6 @@ it('Should expect some values', () => {
     // expect(12).toNotBe(12);
     // expect({name:'Tony'}).toNotEqual({name:'tony'});
     expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toInclude(5);
+
 });
