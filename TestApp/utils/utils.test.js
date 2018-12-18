d00142ef1c9b91cff500e296683899a02741e255
Korpel <Vozikisant@gmail.com>
2018-12-18 19:09:25 +0200

    TestApp/utils/utils.test:

--- TestApp/utils/utils.test.js
+++ TestApp/utils/utils.test.js
@@ -3,6 +3,14 @@ const utils = require('./utils');
 it('Should show 2 number', () => {
     var res = utils.add(33,11);
     if (res !== 44) {
-    throw new Error(`Excpected 44 but got ${res}`);
+    throw new Error(`Excpected 44 but got ${res} `);
     }
-})
+});
+
+it('Should square a number', () => {
+    var res = utils.square(10);
+    if (res !== 99) {
+        throw new Error(`Excpected 100 but instead got ${res}`)
+    }
+
+});
