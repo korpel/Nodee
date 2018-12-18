c297df6be7862a90c62bc20f822b7902a9a1996a
Korpel <Vozikisant@gmail.com>
2018-12-18 19:29:20 +0200

    TestApp/utils/utils.test:

--- TestApp/utils/utils.test.js
+++ TestApp/utils/utils.test.js
@@ -5,6 +5,7 @@ it('Should show 2 number', () => {
     var res = utils.add(33,11);
 
     expect(res).toBe(44).toBeA('number');
+
     // if (res !== 44) {
     // throw new Error(`Excpected 44 but got ${res}`);
     // }
@@ -12,8 +13,11 @@ it('Should show 2 number', () => {
 
 it('Should square a number', () => {
     var res = utils.square(10);
-    if (res !== 100) {
-        throw new Error(`Excpected 100 but instead got ${res}`);
-    }
+
+    expect(res).toBe(100).toBeA('number');
+
+    // if (res !== 100) {
+    //     throw new Error(`Excpected 100 but instead got ${res}`);
+    // }
 
 }); 
