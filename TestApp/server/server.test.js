05dcb56d755ef63d0b91efbd4bc54d9bf0c0909c
Korpel <Vozikisant@gmail.com>
2018-12-20 15:56:42 +0200

    TestApp/server/server.test:

--- TestApp/server/server.test.js
+++ TestApp/server/server.test.js
@@ -4,8 +4,8 @@ var app = require('./server').app;
 
 it('should return hello world response', (done) => {
     request(app)
-    .
     .get('/')
+    .expect(200)
     .expect('Hello World')
     .end(done);
 });
