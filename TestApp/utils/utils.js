140d2014412ee93010fc5e6eb1d044b52268dd61
Korpel <Vozikisant@gmail.com>
2018-12-20 15:36:48 +0200

    TestApp/utils/utils:

--- TestApp/utils/utils.js
+++ TestApp/utils/utils.js
@@ -2,8 +2,8 @@
 module.exports.add = (a,b) => a+b;
 
 module.exports.asyncAdd = (a,b,callback) => {
-    setTimeout(()=>{
-        callback(a,b);
+    setTimeout(()=> {
+        callback(a+b);
     },1000);
 };
 
