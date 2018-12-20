b49a60934c5327ce4910357ab2131858608ad851
Korpel <Vozikisant@gmail.com>
2018-12-20 16:13:35 +0200

    TestApp/server/server.test:

--- TestApp/server/server.test.js
+++ TestApp/server/server.test.js
@@ -6,34 +6,33 @@ var app = require('./server').app;
 
 describe('Server', () => {
     describe('/', ()=> {
-
-    });
-    describe('/', ()=> {
-
-    });
-});
-
-it('should return hello world response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res)=> {
-        expect(res.body).toInclude({
-            error: 'Page not Found!'
+        it('should return hello world response', (done) => {
+            request(app)
+            .get('/')
+            .expect(404)
+            .expect((res)=> {
+                expect(res.body).toInclude({
+                    error: 'Page not Found!'
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
+    });
+    describe('/Users', ()=> {
+        it('Should return my user object', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Tony',
+                    age: 265
+                });
+            })
+            .end(done);
+        });
+    });
 });
 
-it('Should return my user object', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Tony',
-            age: 265
-        });
-    })
-    .end(done);
-});
+
+
