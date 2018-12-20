92dafece4365915d1d246fbc0271deecd5c0dfde
Korpel <Vozikisant@gmail.com>
2018-12-20 16:26:46 +0200

    TestApp/spies/app:

--- TestApp/spies/app.js
+++ TestApp/spies/app.js
@@ -1,10 +1,10 @@
-
+var db = require('./db');
 
 module.exports.handleSignup = (email,password) => {
 
     db.saveUser({
-        email: email,
-        password: password
+        email,
+        password
     });
 
 };
