864df08788e71fcb756f5531586c979ddc045b45
Korpel <Vozikisant@gmail.com>
2018-12-20 16:31:25 +0200

    TestApp/spies/app.test:

--- TestApp/spies/app.test.js
+++ TestApp/spies/app.test.js
@@ -4,7 +4,7 @@ const expect = require('expect');
 describe('App', () => {
     it('should call the spy correctly', ()=>{
         var spy = expect.createSpy();
-        //spy();
+        spy();
         expect(spy).toHaveBeenCalled();
     });
 });
