f01fa84bf7a0207fd6e1e58a779d321626fe17d3
Korpel <Vozikisant@gmail.com>
2018-12-20 15:58:26 +0200

    TestApp/server/server:

--- TestApp/server/server.js
+++ TestApp/server/server.js
@@ -4,7 +4,9 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req,res) => {
-    res.status(404).send({error: 'Page not Found!'});
+    res.status(404).send({
+        error: 'Page not Found!'
+    });
 });
 
 app.listen(3000);
