791a58f76a9dfcfb0a899b269e65e36e6ff02cbf
Korpel <Vozikisant@gmail.com>
2018-12-20 16:00:18 +0200

    TestApp/server/server.test:

--- TestApp/server/server.test.js
+++ TestApp/server/server.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
 
@@ -6,8 +7,6 @@ it('should return hello world response', (done) => {
     request(app)
     .get('/')
     .expect(404)
-    .expect({
-        error: 'Page not Found!'
-    })
+    .expect()
     .end(done);
 });
