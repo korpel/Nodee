63670c0b09f6be9efe47aa0dc5f6666f2d740334
Korpel <Vozikisant@gmail.com>
2018-12-23 21:37:02 +0200

    MongoTODOApi/playground/mongodb-delete:

--- MongoTODOApi/playground/mongodb-delete.js
+++ MongoTODOApi/playground/mongodb-delete.js
@@ -9,26 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to the server');
 
-    // db.collection('Todos').find({
-    //     _id: new ObjectID('5c1fac7d74bb20e5a16acd9c')
-    // }).toArray().then((docs)=>{
-    //     console.log('Todos');
-    //     console.log(JSON.stringify(docs, undefined, 2));
-    // }, (err)=>{
-    //     console.log('Unable to fetch todos', err);
-    // });
-    // db.collection('Todos').find().count().then((count)=>{
-    //     console.log('Todos number');
-    //     console.log(count);
-    // }, (err)=>{
-    //     console.log('Unable to fetch todos', err);
-    // });
-        db.collection('Users').find({name: 'Antonis'}).toArray().then((docs)=> {
-            console.log('Users with name Antonis');
-            console.log(JSON.stringify(docs, undefined, 2));
-        }, (err) => {
-            console.log('Unable to fetch Users', err);
-        });
+    
 
     // db.close();
 });
