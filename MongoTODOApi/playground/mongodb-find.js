6eefb2ef2684b15143464959d467864a9b4a64b0
Korpel <Vozikisant@gmail.com>
2018-12-23 20:55:02 +0200

    MongoTODOApi/playground/mongodb-find:

--- MongoTODOApi/playground/mongodb-find.js
+++ MongoTODOApi/playground/mongodb-find.js
@@ -3,38 +3,13 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
 
-// var obj = new ObjectID();
-// console.log(obj);
-// var user = {name : 'Antonis', age: 25};
-// var {name} = user;
-// console.log(name);
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
        return console.log('Unable to connect to the server');
     }
     console.log('Connected to the server');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // },(err, result) => {
-    //     if (err) {
-    //       return  console.log('Unable to insert Todos', err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    // db.collection('Users').insertOne({
-    //     name: 'Antonis',
-    //     age: 25,
-    //     location: 'Loutsa'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert Users', err);
-    //     }
-    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
-    // });
+    db.collection('Todos').find();
 
     db.close();
 });
