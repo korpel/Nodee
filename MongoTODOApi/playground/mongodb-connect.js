2c7edb1422b5f49637b73c294bc72180f4ff69ff
Korpel <Vozikisant@gmail.com>
2018-12-23 17:43:51 +0200

    MongoTODOApi/playground/mongodb-connect:

--- MongoTODOApi/playground/mongodb-connect.js
+++ MongoTODOApi/playground/mongodb-connect.js
@@ -8,15 +8,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to the server');
 
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
-    },(err, result) => {
-        if (err) {
-          return  console.log('Unable to insert Todos', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // },(err, result) => {
+    //     if (err) {
+    //       return  console.log('Unable to insert Todos', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
 
     db.collection('Users').insertOne({
         name: 'Antonis',
