ef3204193fae959dc08e6be428347a7a9f342a2a
Korpel <Vozikisant@gmail.com>
2018-12-23 20:57:59 +0200

    MongoTODOApi/playground/mongodb-find:

--- MongoTODOApi/playground/mongodb-find.js
+++ MongoTODOApi/playground/mongodb-find.js
@@ -11,7 +11,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     db.collection('Todos').find().toArray().then((docs)=>{
         console.log('Todos');
-        
+        console.log(JSON.stringify(docs, undefined, 2));
     }, (err)=>{
         console.log('Unable to fetch todos', err);
     });
