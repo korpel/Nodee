b8caaff515687433886c4667b301440d8be78744
Korpel <Vozikisant@gmail.com>
2018-12-23 17:31:41 +0200

    MongoTODOApi/playground/mongodb-connect:

--- MongoTODOApi/playground/mongodb-connect.js
+++ MongoTODOApi/playground/mongodb-connect.js
@@ -4,6 +4,11 @@ const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
-        console.log('Unable to connect to the server');
+       return console.log('Unable to connect to the server');
     }
+    console.log('Connected to the server');
+
+
+
+    db.close();
 });
