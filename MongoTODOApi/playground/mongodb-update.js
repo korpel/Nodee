a383f1f61ea6c2e3348c608f9b69fbdd977b5613
Korpel <Vozikisant@gmail.com>
2018-12-24 08:09:39 +0200

    MongoTODOApi/playground/mongodb-update:

--- MongoTODOApi/playground/mongodb-update.js
+++ MongoTODOApi/playground/mongodb-update.js
@@ -9,7 +9,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to the server');
 
-    
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID("5c1fe4d51b0812390567b328")
+    })
 
     // db.close();
 });
