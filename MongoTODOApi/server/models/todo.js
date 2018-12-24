5908ccddf81b06ab80e85e944cc35b3d82ae19cc
Korpel <Vozikisant@gmail.com>
2018-12-24 10:07:30 +0200

    MongoTODOApi/server/models/todo:

---
+++ MongoTODOApi/server/models/todo.js
@@ -0,0 +1,15 @@
+var Todo = mongoose.model('Todo', {
+    text : {
+        type: String,
+        required : true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number,
+        default : null
+    }
+});
