7a576de9be1b31d61f338c009c45013c5adc195b
Korpel <Vozikisant@gmail.com>
2018-12-24 08:28:37 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -3,3 +3,15 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text : {
+
+    },
+    completed: {
+
+    },
+    completedAt: {
+
+    }
+});
