eb07607e0c2b815b1617721278a4a6d83d159d5c
Korpel <Vozikisant@gmail.com>
2018-12-25 08:01:34 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -4,3 +4,17 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models');
+
+describe('POST /todos', () => {
+    it('Should create new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+        .post('/todos')
+        .send(text)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text);
+        });
+    });
+});
