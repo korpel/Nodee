4ce4ea0902dcbb44798bcff15674aca1bfa15f49
Korpel <Vozikisant@gmail.com>
2018-12-30 15:19:23 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -60,5 +60,12 @@ describe('POST /todos', () => {
 });
 
 describe('GET /todos', ()=>{
-
+it('Should get all todos', (done)=>{
+    request(app)
+    .get('todos')
+    .expect(200)
+    .expect((res)=> {
+        expect(res.body.todos.length).toBe(2)
+    })
+});
 });
