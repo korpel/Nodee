321b136c710b84ae782dc88383126e21342c56cb
Korpel <Vozikisant@gmail.com>
2019-01-02 15:30:05 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -40,8 +40,10 @@ app.get('/todos/:id',(req, res)=>{
     if (!ObjectID.isValid(id)) {
         res.status(404).send();
     }
-    Todo.findById(id).then(()=> {
-
+    Todo.findById(id).then((todo)=> {
+        if (!todo) {
+            res.status(404).send();
+        }
     });
 });
 
