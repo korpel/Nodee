a45ea1a21d1362cb25149c27fc9a13629396963a
Korpel <Vozikisant@gmail.com>
2019-01-02 15:14:04 +0200

    MongoTODOApi/playground/mongoose-queries:

--- MongoTODOApi/playground/mongoose-queries.js
+++ MongoTODOApi/playground/mongoose-queries.js
@@ -5,17 +5,17 @@ const {Todo} = require('./../server/models/todo');
 
 var id = '5c28c642fa5e42054094303f';
 
-Todo.find({
-    _id : id
-}).then((todos)=> {
-    console.log('Todos', todos)
-}); 
+// Todo.find({
+//     _id : id
+// }).then((todos)=> {
+//     console.log('Todos', todos)
+// }); 
 
-Todo.findOne({
-    _id : id
-}).then((todo)=> {
-    console.log('Todo', todo)
-}); 
+// Todo.findOne({
+//     _id : id
+// }).then((todo)=> {
+//     console.log('Todo', todo)
+// }); 
 
 Todo.findById(id).then((todo)=> {
     if (!todo) {
