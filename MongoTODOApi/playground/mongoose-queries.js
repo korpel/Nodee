14cc7e4e4ddcc83a18b8d9e3cbb8df17071b5f42
Korpel <Vozikisant@gmail.com>
2019-01-02 15:08:22 +0200

    MongoTODOApi/playground/mongoose-queries:

--- MongoTODOApi/playground/mongoose-queries.js
+++ MongoTODOApi/playground/mongoose-queries.js
@@ -1,3 +1,6 @@
 
 const {mongoose} = require('./../server/db/mongoose');
-const {todo} = require('./../server/models/todo');
+const {Todo} = require('./../server/models/todo');
+
+
+var id = '5c28c642fa5e42054094303f';
