48132b1561360976ed35a07e878ad0f88bcf3f85
Korpel <Vozikisant@gmail.com>
2019-01-03 18:45:08 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -64,7 +64,9 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send(todo);
-    }).catch((e)=>res.status(400).send());
+    }).catch((e)=>{
+        res.status(400).send()
+    });
 });
 
 
