4f5ee5730de91ca4848369acea6cffc792656370
Korpel <Vozikisant@gmail.com>
2019-01-03 20:44:57 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -79,9 +79,9 @@ app.patch('/todos.:id', (req,res) => {
     }
 
     if (_.isBoolean(body.completed) && body.completed) {
-
+        body.completedAt = new Date().getTime();
     } else {
-        
+
     }
 });
 
