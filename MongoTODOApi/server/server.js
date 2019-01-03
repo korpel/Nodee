9df84c4d577a9507df98cce2f9cbd284ba7b1275
Korpel <Vozikisant@gmail.com>
2019-01-04 01:28:46 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -7,7 +7,6 @@ if (env === 'development'){
 }else if (env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-
 }
 
 const express = require('express');
