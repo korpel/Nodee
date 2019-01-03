ee0ac813610d056505ccf8a205c8b97447ef1e04
Korpel <Vozikisant@gmail.com>
2019-01-03 20:25:45 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -104,7 +104,7 @@ describe('GET /todos/:id', ()=> {
 
 describe('DELETE /todos/:id', () => {
   it('Should remove a todo', (done)=>{
-
+    var hexID = todos[1]._id.toHexString();
   });
   
   it('Sould return 404 if todo not found', (done) => {
