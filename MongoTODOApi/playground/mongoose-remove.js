c2b9e840017abd3221f785d0232d2bb37aa27f67
Korpel <Vozikisant@gmail.com>
2019-01-03 18:25:17 +0200

    MongoTODOApi/playground/mongoose-remove:

--- MongoTODOApi/playground/mongoose-remove.js
+++ MongoTODOApi/playground/mongoose-remove.js
@@ -8,3 +8,7 @@ const {User} = require ('./../server/models/user');
 // Todo.remove({}).then( (result)=> {
 //     console.log(result);
 // });
+
+Todo.findByIdAndRemove('5c2e373a1b08123905680c53').then((result)=> {
+    console.log(result);
+});
