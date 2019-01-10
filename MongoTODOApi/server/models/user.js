a03e25855ec24844a1d16cf90a7641bdbcb4a590
Korpel <Vozikisant@gmail.com>
2019-01-10 12:52:02 +0200

    MongoTODOApi/server/models/user:

--- MongoTODOApi/server/models/user.js
+++ MongoTODOApi/server/models/user.js
@@ -36,6 +36,8 @@ UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access,token}]);
 }
 
 var User = mongoose.model('Users', UserSchema);
