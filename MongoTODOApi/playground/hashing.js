094273a41a95f1c1eec0d7733b5ac46c35b0ab48
Korpel <Vozikisant@gmail.com>
2019-01-10 07:10:16 +0200

    MongoTODOApi:

--- MongoTODOApi/playground/hashing.js
+++ MongoTODOApi/playground/hashing.js
@@ -1,24 +1,24 @@
 const {SHA256} = require('crypto-js');
 
-var message = 'I am number 3';
+// var message = 'I am number 3';
 
-var hash = SHA256(message).toString();
+// var hash = SHA256(message).toString();
 
-var data = {
-    id: 4
-};
+// var data = {
+//     id: 4
+// };
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}; 
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }; 
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(data) + 'a').toString()
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(data) + 'a').toString()
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString()
-if (resultHash === token.hash) {
-    console.log('Data was not changed!');
-} else {
-    console.log('Data was changed. Do not trust');
-}
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString()
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed!');
+// } else {
+//     console.log('Data was changed. Do not trust');
+// }
