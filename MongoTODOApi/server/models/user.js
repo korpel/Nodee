808f70edad8d3b774c4caf281efdc98eb9dd2be4
Korpel <Vozikisant@gmail.com>
2019-01-10 12:47:11 +0200

    MongoTODOApi/server/models/user:

--- MongoTODOApi/server/models/user.js
+++ MongoTODOApi/server/models/user.js
@@ -1,34 +1,36 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-var User = mongoose.model('Users', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true,
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email!'
-        }
-    },
-    password : {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    tokens : [{
-        access: {
+var userSchema = mongoose.Schema({
+        email: {
             type: String,
-            required: true
+            required: true,
+            minlength: 1,
+            trim: true,
+            unique: true,
+            validate: {
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email!'
+            }
         },
-        token: {
+        password : {
             type: String,
-            required: true
-        }
-
-    }]
+            required: true,
+            minlength: 6
+        },
+        tokens : [{
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+    
+        }]
 });
+
+var User = mongoose.model('Users', userSchema);
 
 module.exports = {User};
