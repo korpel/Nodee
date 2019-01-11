2152594ce2e522914e76c124eefc0274d99a8c6f
Korpel <Vozikisant@gmail.com>
2019-01-11 07:59:01 +0200

    MongoTODOApi/playground/hashing:

--- MongoTODOApi/playground/hashing.js
+++ MongoTODOApi/playground/hashing.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 var password = '123abc';
 bcrypt.genSalt(10,(err, salt)=>{
     bcrypt.hash(password, salt,(err,hash)=>{
-
+        console.log(hash);
     });
 });
 
