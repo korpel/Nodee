4439669409d90718c34be295896122d8fa2c1f48
Korpel <Vozikisant@gmail.com>
2019-01-11 08:16:19 +0200

    MongoTODOApi/server/tests/seed/seed:

--- MongoTODOApi/server/tests/seed/seed.js
+++ MongoTODOApi/server/tests/seed/seed.js
@@ -1,6 +1,6 @@
 const {ObjectID} = require('mongodb');
 const {Todo} = require('./../../models/todo');
-
+const {User}= require('./../../models/user');
 const todos = [{
     _id : new ObjectID(),
     text:'First test todo'
