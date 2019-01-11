ff545df45edf7debadd706c3d21add40236621c6
Korpel <Vozikisant@gmail.com>
2019-01-11 08:13:27 +0200

    MongoTODOApi/server/tests/seed/seed:

--- MongoTODOApi/server/tests/seed/seed.js
+++ MongoTODOApi/server/tests/seed/seed.js
@@ -1,3 +1,6 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../models/todo');
+
 const todos = [{
     _id : new ObjectID(),
     text:'First test todo'
@@ -7,3 +10,9 @@ const todos = [{
     completed: true,
     completedAt: 333
 }];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+       return Todo.insertMany(todos);
+    }).then(() => done());
+  }
