d8a4804868c06cd3b875d50ea93616b920b65907
Korpel <Vozikisant@gmail.com>
2019-01-11 08:27:07 +0200

    MongoTODOApi/server/tests/seed/seed:

--- MongoTODOApi/server/tests/seed/seed.js
+++ MongoTODOApi/server/tests/seed/seed.js
@@ -44,6 +44,10 @@ const populateUsers = (done) => {
         var userOne = new User(users[0]).save();
         var userTwo = new User(users[1]).save();
 
+        Promise.all([userOne,userTwo]).then(()=>{
+
+        });
+
     });
 };
 
