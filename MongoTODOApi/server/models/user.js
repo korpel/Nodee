e99c4b31345a6a22ab24ace1065a08896bce438d
Korpel <Vozikisant@gmail.com>
2019-01-11 08:05:43 +0200

    MongoTODOApi/server/models/user:

--- MongoTODOApi/server/models/user.js
+++ MongoTODOApi/server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = mongoose.Schema({
         email: {
