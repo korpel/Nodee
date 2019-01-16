8f4619dba981355f8d9c715c4b02b9b003e152d4
Korpel <Vozikisant@gmail.com>
2019-01-16 22:19:47 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -5,6 +5,7 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user')
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 
