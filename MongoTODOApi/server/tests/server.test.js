02e8085a8c75806ba42627ab70463bf28d4244cc
Korpel <Vozikisant@gmail.com>
2019-02-01 06:09:39 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -242,7 +242,7 @@ describe('POST /users', ()=>{
 
     it('Should not create user if email in use',(done)=>{
       request(app)
-      .post('/us ers')
+      .post('/users')
       .send({
         'email':users[0].email,
         'password': 'somePass'
