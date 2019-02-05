e9353f4a3ca2cab3d39a0736f170063a7d06e57e
Korpel <Vozikisant@gmail.com>
2019-02-05 06:18:39 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -261,6 +261,7 @@ describe(' GET /users/me', ()=>{
       expect(res.body._id).toBe(users[0]._id.toHexString());
       expect(res.body.email).toBe(users[0].email);
     })
+    .end(done);
   });
   it('Should return 401 if not authenticated',(done)=>{
 
