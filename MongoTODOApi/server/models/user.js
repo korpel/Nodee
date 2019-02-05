fc689587af781bc5781ecc4798a1355e22bc6810
Korpel <Vozikisant@gmail.com>
2019-02-05 06:47:56 +0200

    MongoTODOApi/server/models/user:

--- MongoTODOApi/server/models/user.js
+++ MongoTODOApi/server/models/user.js
@@ -61,7 +61,9 @@ UserSchema.statics.findByCredentials = function (email, password){
 
         return new Promise((resolve,reject)=>{
             bcrypt.compare(password, user.password, (err,res)=>{
-                if (res)
+                if (res){
+                    resolve(user);
+                }
             });
         });
 
