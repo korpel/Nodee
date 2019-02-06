65a2e66bbe609584bc2fae1f420aa663da856714
Korpel <Vozikisant@gmail.com>
2019-02-06 09:08:03 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -391,7 +391,7 @@ describe('POST /users/login', (done) => {
     })
     .expect(200)
     .expect((res)=> {
-      expect(res.headers['x-auth']).toExist();
+      expect(res.headers['x-auth']).toBeTruthy();
     })
     .end((err, res)=> {
       if (err) {
