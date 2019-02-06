231ceb1cd350bee74bb5a2122d4e24b2d7bc22b0
Korpel <Vozikisant@gmail.com>
2019-02-06 08:26:37 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -71,7 +71,7 @@ app.get('/todos/:id',authenticate,(req, res)=>{
     });
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
