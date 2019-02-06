6a8c8ab924a64d8861b6876b3fe390fc6cf6991f
Korpel <Vozikisant@gmail.com>
2019-02-06 08:01:21 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -390,7 +390,7 @@ describe('DELETE /users/me/token',() => {
       User.findById(users[0]._id).then((user)=>{
         expect(user.tokens.length).toBe(0);
         done();
-      })
+      }).catch((e)=>done(e));
     });
   });
 
