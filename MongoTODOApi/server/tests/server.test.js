037a70cb2b12b56f8ff88f487b9cbce5934dc1cd
Korpel <Vozikisant@gmail.com>
2019-02-06 09:04:25 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -188,7 +188,8 @@ describe('PATCH /todos/:id', () => {
     .expect((res)=> {
       expect(res.body.todo.text).toBe(text);
       expect(res.body.todo.completed).toBe(true);
-      expect(res.body.todo.completedAt).toBeA('number');
+      //expect(res.body.todo.completedAt).toBeA('number');
+      expect(typeof res.body.todo.completedAt).toBe('number')
     })
     .end(done)
   });
