bd6d24cb67744034346adf67415bce3327f27ec4
Korpel <Vozikisant@gmail.com>
2019-02-06 09:09:00 +0200

    MongoTODOApi/server/tests/server.test:

--- MongoTODOApi/server/tests/server.test.js
+++ MongoTODOApi/server/tests/server.test.js
@@ -399,7 +399,7 @@ describe('POST /users/login', (done) => {
       }
 
     User.findById(users[1]._id).then((user)=> {
-      expect(user.tokens[1]).toInclude({
+      expect(user.toObeject().tokens[1]).toInclude({
         access: 'auth',
         token: res.headers['x-auth']
       });
