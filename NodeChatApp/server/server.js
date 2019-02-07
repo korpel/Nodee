0ec426a4f1638af1440a40d95c7ba8e1ef289a70
Korpel <Vozikisant@gmail.com>
2019-02-07 07:13:30 +0200

    sa

--- NodeChatApp/server/server.js
+++ NodeChatApp/server/server.js
@@ -3,26 +3,32 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection',(socket)=>{
-    console.log('new user connected');
-   
- socket.on('disconnect',()=>{
-    console.log('User disconnected');
-   });
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hey. What is going on.',
+    createAt: 123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-
-
-server.listen(port,()=>{
-    console.log(`Server is up on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
