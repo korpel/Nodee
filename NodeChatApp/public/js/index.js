8796a589ac6accb9a1746dce5e4f663615e13f59
Korpel <Vozikisant@gmail.com>
2019-02-07 07:14:36 +0200

    asadas

--- NodeChatApp/public/js/index.js
+++ NodeChatApp/public/js/index.js
@@ -1,18 +1,10 @@
 var socket = io();
 
-socket.on('connect', function () {
-  console.log('Connected to server');
-
-  socket.emit('createEmail', {
-    to: 'jen@example.com',
-    text: 'Hey. This is Andrew.'
-  });
+socket.on('connect', function (){
+    console.log('Connected to server');
 });
+
 
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
-
-socket.on('newEmail', function (email) {
-  console.log('New email', email);
+    console.log('Disconnected from server');
 });
