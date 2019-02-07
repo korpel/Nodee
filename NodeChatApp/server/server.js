ea978b8231ae5bd261f6c21fb5118c3f72d7c0ce
Korpel <Vozikisant@gmail.com>
2019-02-07 06:34:09 +0200

    NodeChatApp/server/server:

--- NodeChatApp/server/server.js
+++ NodeChatApp/server/server.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
-var io = socketIO(server).listen(server);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
 
