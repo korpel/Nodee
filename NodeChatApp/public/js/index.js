136d21801ac338e2fce4e60caef6e8d9fe3543cc
Korpel <Vozikisant@gmail.com>
2019-02-07 07:15:22 +0200

    NodeChatApp/public/js/index:

--- NodeChatApp/public/js/index.js
+++ NodeChatApp/public/js/index.js
@@ -1,10 +1,10 @@
-var socket = io();
+// var socket = io();
 
-socket.on('connect', function (){
-    console.log('Connected to server');
-});
+// socket.on('connect', function (){
+//     console.log('Connected to server');
+// });
 
 
-socket.on('disconnect', function () {
-    console.log('Disconnected from server');
-});
+// socket.on('disconnect', function () {
+//     console.log('Disconnected from server');
+// });
