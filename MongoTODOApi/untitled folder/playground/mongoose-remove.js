242f2e7b21077bdbe1c7766b57b7dcd1a6a39a52
Korpel <Vozikisant@gmail.com>
2019-02-07 07:09:26 +0200

    a

---
+++ MongoTODOApi/untitled folder/playground/mongoose-remove.js
@@ -0,0 +1,14 @@
+
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+const {ObjectID} = require('mongodb');
+const {User} = require ('./../server/models/user');
+
+// Todo.remove({}).then( (result)=> {
+//     console.log(result);
+// });
+
+// Todo.findByIdAndRemove('5c2e373a1b08123905680c53').then((todo)=> {
+//     console.log(todo);
+// });
