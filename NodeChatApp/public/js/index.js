456a56406922ef31eb375c28a04c5babc56a4515
Korpel <Vozikisant@gmail.com>
2019-02-08 07:20:19 +0200

    NodeChatApp/public/js/index:

--- NodeChatApp/public/js/index.js
+++ NodeChatApp/public/js/index.js
@@ -20,6 +20,8 @@ var socket = io();
     socket.on('newLocationMessage', function(message) {
         var li = jQuery('<li></li>');
         var a = jQuery('<a target="_blank>My current location</a>');
+
+        li.text(`${message.from}: `);
     });
 
     // socket.emit('createMessage', {
