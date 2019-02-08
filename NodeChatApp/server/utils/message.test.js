4aeffe98796b7f4a8ca7ee3c0be3ef52ee99a652
Korpel <Vozikisant@gmail.com>
2019-02-08 06:27:07 +0200

    NodeChatApp/server/utils/message.test:

--- NodeChatApp/server/utils/message.test.js
+++ NodeChatApp/server/utils/message.test.js
@@ -4,6 +4,8 @@ var {generateMessage} = require('./message');
 
 describe('generateMessage',() => {
     it('Should generate the correct message object', () => {
-
+        var from = 'Jen';
+        var text = 'Some message'
+        var message = generateMessage(from, text);
     });
 });
