385ff8e4a70408a500b219796b8bb406f28d2396
Korpel <Vozikisant@gmail.com>
2019-02-08 06:03:23 +0200

    NodeChatApp/server/server:

--- NodeChatApp/server/server.js
+++ NodeChatApp/server/server.js
@@ -25,7 +25,9 @@ io.on('connection',(socket)=>{
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
-
+            from: message.from,
+            text: message.text
+            createdAt: new Date().getTime();
         });
     });
    
