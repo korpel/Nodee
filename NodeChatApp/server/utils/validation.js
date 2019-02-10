4562c4494f52774f647bc328ec27e4e0ae6d3498
Korpel <Vozikisant@gmail.com>
2019-02-10 07:38:40 +0200

    NodeChatApp/server/utils/validation:

--- NodeChatApp/server/utils/validation.js
+++ NodeChatApp/server/utils/validation.js
@@ -1,4 +1,9 @@
+import { builtinModules } from "module";
+
 var isRealString = (str) => {
     return typeof str === 'string' && str.trim().length > 0;
     ;
 };
+
+
+module.exports = {isRealString};
