e2f49f7e856cbca530556410c87cbbd95461c15b
Korpel <Vozikisant@gmail.com>
2019-02-10 07:35:41 +0200

    NodeChatApp/server/server:

--- NodeChatApp/server/server.js
+++ NodeChatApp/server/server.js
@@ -24,6 +24,9 @@ io.on('connection',(socket)=>{
     socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
 
+    socket.on('join')
+
+
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
