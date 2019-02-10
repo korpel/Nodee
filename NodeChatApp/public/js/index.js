2ca7871bf9ec3e3b2e27488d36e0a4e72f64c542
Korpel <Vozikisant@gmail.com>
2019-02-10 07:11:49 +0200

    NodeChatApp/public/js/index:

--- NodeChatApp/public/js/index.js
+++ NodeChatApp/public/js/index.js
@@ -9,7 +9,7 @@ function scrollToBottom() {
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
 
-    if (clientHeight+screenTop  +newMessageHeight + lastMessageHeight>= scrollHeight) {
+    if (clientHeight+scrollTop  +newMessageHeight + lastMessageHeight>= scrollHeight) {
         messages.scrollTop(scrollHeight);
     }
 
