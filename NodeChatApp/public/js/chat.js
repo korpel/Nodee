ed852eba15a45a0369db7b033b023f0334f04bd1
Korpel <Vozikisant@gmail.com>
2019-02-10 07:33:47 +0200

    NodeChatApp/public/js/chat:

--- NodeChatApp/public/js/chat.js
+++ NodeChatApp/public/js/chat.js
@@ -16,7 +16,7 @@ function scrollToBottom() {
 };
 
     socket.on('connect', function (){
-        console.log('Connected to server');
+        var params = jQuery.deparam(window.location.search);
     });
 
     socket.on('disconnect', function () {
