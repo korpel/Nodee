e8e05f65ae1ca023396c57ac6cf22bbe58ba5e53
Korpel <Vozikisant@gmail.com>
2019-02-11 06:55:03 +0200

    NodeChatApp/server/server:

--- NodeChatApp/server/server.js
+++ NodeChatApp/server/server.js
@@ -30,7 +30,8 @@ io.on('connection', (socket) => {
       users.addUser(socket.id, params.name, params.room);
 
 
-      io.to(params.room).emit('updateUserList');
+      io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
       socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
       socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
 
