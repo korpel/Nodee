50b1a461318fad93ccf7f10752315e9a396fe70a
Korpel <Vozikisant@gmail.com>
2019-02-11 07:05:49 +0200

    asd

--- NodeChatApp/public/js/chat.js
+++ NodeChatApp/public/js/chat.js
@@ -33,9 +33,14 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
 
-socket.on('updateUserList', function (users){
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
 
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
