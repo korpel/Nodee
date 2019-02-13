30e130c08e23ad74dc3c55d5d255fd8c45229703
Korpel <Vozikisant@gmail.com>
2019-02-13 06:47:33 +0200

    Async-Await/app-promises:

--- Async-Await/app-promises.js
+++ Async-Await/app-promises.js
@@ -72,16 +72,32 @@ const getStatus = ((userId)=>{
 // });
 
 
+// const getStatusAlt = async (userId) => {
+//     const user = await getUser(userId);
+//     const grades = await getGrades(userId.schoolId);
+//     let average = 0;
+
+//     if (grades.length > 0) {
+//        average = grades.map((grade) => grade.grade).reduce((a,b)=> a+b)/ grades.length;
+//         }
+    
+//     return `${user.name} has a ${average} in the class`
+// };
+
+
+
 const getStatusAlt = async (userId) => {
     const user = await getUser(userId);
-    const grades = await getGrades(userId.schoolId);
+    const grades = await getGrades(user.schoolId);
     let average = 0;
-
+  
     if (grades.length > 0) {
-       average = grades.map((grade) => grade.grade).reduce((a,b)=> a+b)/ grades.length;
-        }
-        return `${user.name} has a ${average} in the class`
-};
+      average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+    }
+  
+    return `${user.name} has a ${average}% in the class.`;
+  };
+  
 
 getStatusAlt(2).then((status)=>{
     console.log(status);
