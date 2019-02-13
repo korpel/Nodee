7a96cb8e29fab36508ef305581b9257a0e5241e0
Korpel <Vozikisant@gmail.com>
2019-02-13 07:18:48 +0200

    Async-Await/currency-convert:

--- Async-Await/currency-convert.js
+++ Async-Await/currency-convert.js
@@ -2,8 +2,8 @@ const {key, url} = require('./my-key');
 const axios = require('axios');
 
 
-const getExhangeRate = (from, to)=> {
-  return  axios.get(url).then(()=>{
+const getExhangeRate = (from, to) => {
+  return  axios.get(url).then((response)=>{
         const euro = 1 / response.data.rates[from];
         const rate = euro * response.data.rates[to];
         return rate;
