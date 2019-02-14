976f4a28f4df37aa1c9fd9b0f1e1fdfb37cff4ec
Korpel <Vozikisant@gmail.com>
2019-02-14 06:13:27 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -142,7 +142,8 @@ app.get('/users/me',authenticate, (req, res)=>{
 app.post('/users/login', async (req,res) =>{ 
     const body = _.pick(req.body, ['email', 'password']);
     const user = await User.findByCredentials(body.email, body.password);
-
+    const token = await user.generateAuthToken();
+    
     User.findByCredentials(body.email, body.password).then((user)=>{
         user.generateAuthToken().then((token)=>{
             res.header('x-auth', token).send(user);
