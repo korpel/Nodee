c8751208106f31cd6467a4f6e547ddedbe97f05a
Korpel <Vozikisant@gmail.com>
2019-02-14 06:10:26 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -153,7 +153,7 @@ app.post('/users/login',(req,res) =>{
 
 });
 
-app.delete('/users/me/token', authenticate,(req,res)=>{
+app.delete('/users/me/token', authenticate, async (req,res)=>{
     req.user.removeToken(req.token).then(()=>{
         res.status(200).send();
     },()=>{
