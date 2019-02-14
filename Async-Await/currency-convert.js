6c0090de0c9e66dcfc7569593462404ad133b65b
Korpel <Vozikisant@gmail.com>
2019-02-14 06:01:59 +0200

    Async-Await/currency-convert:

--- Async-Await/currency-convert.js
+++ Async-Await/currency-convert.js
@@ -18,8 +18,8 @@ const getExhangeRate =  async (from, to) => {
         const euro = 1 / response.data.rates[from];
         const rate = euro * response.data.rates[to];
         return rate;
-    } catch {
-
+    } catch (e) {
+        throw new Error('Kati problima epaikse sth metatropi');
     }
   };
 
