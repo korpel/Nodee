8566c539c5aed47159134c4005fc4870525433d4
Korpel <Vozikisant@gmail.com>
2019-02-14 05:45:13 +0200

    Async-Await/currency-convert:

--- Async-Await/currency-convert.js
+++ Async-Await/currency-convert.js
@@ -28,7 +28,7 @@ const getCountries = async (currencyCode)=>{
 
 const convertCurrency = (from,to,amount) => {
     getExhangeRate(from,to).then((rate)=> {
-
+        const convertedAmount = (amount*rate).toFixed(2);
     });
 };
 
