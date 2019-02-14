cb234b6371fe81436e9ae67d74506c594f77272c
Korpel <Vozikisant@gmail.com>
2019-02-14 06:12:25 +0200

    MongoTODOApi/server/server:

--- MongoTODOApi/server/server.js
+++ MongoTODOApi/server/server.js
@@ -139,7 +139,7 @@ app.get('/users/me',authenticate, (req, res)=>{
     res.send(req.user);
 });
 
-app.post('/users/login',(req,res) =>{ 
+app.post('/users/login', async (req,res) =>{ 
     var body = _.pick(req.body, ['email', 'password']);
 
     User.findByCredentials(body.email, body.password).then((user)=>{
